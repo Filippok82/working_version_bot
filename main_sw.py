620f55bba9d9c4d8026c0bd9962ac0ecf2e32f72
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-03-04 20:42:00 +0300

    Working version bot

---
+++ main_sw.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from commands_sw import *
+
+
+
+updater = Updater('')
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello_command))
+updater.dispatcher.add_handler(CommandHandler('m', play_man))
+updater.dispatcher.add_handler(CommandHandler('b', play_bot))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, input_handler))
+
+
+print('server start')
+
+updater.start_polling()
+updater.idle()
